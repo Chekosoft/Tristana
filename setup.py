d3e034700d50af5fbdd7bbcdf2ab089980655c40
Joaquín Muñoz M <jemunozmunoz@gmail.com>
2015-08-10 18:59:37 -0300

    Version bump to 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name=u'Tristana',
-    version=u'0.1.1',
+    version=u'0.1.2',
     description=u'A layer to build Tornado apps at a faster rate.',
     install_requires = [
         'tornado>=4.2.1'
